37d8b9affda2c4d9c10df6ffb97cdb37c073ffe9
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2024-04-20 15:50:20 +0530

    Added htop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -119,6 +119,7 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      git
+     htop
      tmux
      file
      unzip
